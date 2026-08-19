6298cc25d6272cf7396cfdc348e84d1359146f60
mac <sabondojoshua8@gmail.com>
2026-08-19 14:33:30 +0200

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["firebasestorage.googleapis.com"],
-  },
-};
-
-export default nextConfig;
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
     remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
