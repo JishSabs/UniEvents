02a0eaff54dcd699d4fe2ae0367473a80b24bd88
mac <sabondojoshua8@gmail.com>
2026-08-19 14:27:39 +0200

    updated image loading code in confimjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,3 +6,17 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
